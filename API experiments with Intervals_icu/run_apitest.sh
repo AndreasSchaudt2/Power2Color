f7e0e2cccc9446fb07869d4dcbb3ea1650b1ad29
Andreas Schaudt <Andrears.Schaudt@gmx.de>
2024-11-11 08:26:31 +0100

    removed more old files

--- API experiments with Intervals_icu/run_apitest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source /home/andi/myenv/bin/activate
-
-# Run the Python script with sudo
-sudo /home/andi/myenv/bin/python /home/andi/Code/api_tst.py
